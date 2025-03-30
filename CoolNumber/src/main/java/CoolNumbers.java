d1b26b15d52224d0e37cc81a6d87ab2236fa963e
Smotrov14 <smotrov144@gmail.com>
2025-03-30 19:59:52 +0300

    Готово

--- CoolNumber/src/main/java/CoolNumbers.java
+++ CoolNumber/src/main/java/CoolNumbers.java
@@ -1,29 +1,58 @@
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.List;
-import java.util.TreeSet;
+import java.util.*;
 
 public class CoolNumbers {
+    private static final char[] ALLOWED_LETTERS = {
+            'А', 'В', 'Е', 'К', 'М', 'Н', 'О', 'Р', 'С', 'Т', 'У', 'Х'};
 
     public static List<String> generateCoolNumbers() {
-        return Collections.emptyList();
+        List<String>numbers= new ArrayList<>();
+        for(char X: ALLOWED_LETTERS){
+            for (int N = 0; N < 9; N++) {
+                for(char Y:ALLOWED_LETTERS){
+                    for(char Z:ALLOWED_LETTERS){
+                        for (int i = 1; i <199 ; i++) {
+                            String R = i < 10 ? "0" + i : String.valueOf(i);
+                            String result = String.format("%c%d%d%d%c%c%s", X, N, N, N, Y, Z, R);
+                            numbers.add(result);
+                        }
+                    }
+                }
+            }
+        }
+        Collections.shuffle(numbers);
+        return numbers;
     }
 
     public static boolean bruteForceSearchInList(List<String> list, String number) {
-        return false;
+        boolean isHave = list.contains(number);
+        print(isHave,"Поиск перебором: номер");
+        return isHave;
     }
 
     public static boolean binarySearchInList(List<String> sortedList, String number) {
-        return false;
+        Collections.sort(sortedList);
+        int index = Collections.binarySearch(sortedList ,number);
+        boolean isHave = index >= 0;
+        print(isHave,"Бинарный поиск: номер");
+        return isHave;
     }
 
 
     public static boolean searchInHashSet(HashSet<String> hashSet, String number) {
-        return false;
+        boolean isHave = hashSet.contains(number);
+        print(isHave,"Поиск в HashSet: номер");
+        return isHave;
     }
 
     public static boolean searchInTreeSet(TreeSet<String> treeSet, String number) {
-        return false;
+        boolean isHave = treeSet.contains(number);
+        print(isHave,"Поиск в TreeSet: номер");
+        return isHave;
     }
-
+        public static void print(boolean isTrue,String metod){
+            long start = System.nanoTime();
+            long end = System.nanoTime();
+            String result = isTrue? "найден" : "не найден";
+            System.out.println(metod + " " + result + ", поиск занял "+ (end - start) + " нс");
+        }
 }
--- CoolNumber/src/main/java/Main.java
+++ CoolNumber/src/main/java/Main.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 public class Main {
     /*
     TODO:
@@ -7,6 +9,15 @@ public class Main {
      */
 
     public static void main(String[] args) {
+        List<String> arrayList = CoolNumbers.generateCoolNumbers();
+        System.out.println(arrayList.get(7));
+        System.out.println(arrayList.size());
+        CoolNumbers.binarySearchInList(arrayList, "Т333ВВ201");
+        CoolNumbers.bruteForceSearchInList(arrayList, "Т333ВВ171");
+        TreeSet<String> treeSet = new TreeSet<>(CoolNumbers.generateCoolNumbers());
+        CoolNumbers.searchInTreeSet( treeSet, "Т333ВВ171");
+        HashSet<String> hashSet = new HashSet<>(CoolNumbers.generateCoolNumbers());
+        CoolNumbers.searchInHashSet(hashSet,"Т333ВВ171");
 
     }
 }
